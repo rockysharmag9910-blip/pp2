c4957190239c6a9f751f6fb9ecb2f8133e9ee9fb
rockysharmag9910-blip <rockysharmag9910@gmail.com>
2026-02-28 20:12:53 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,6 @@
 self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
-
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
