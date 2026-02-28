f16bc6c5668503125741d55b2433391a0584136b
rockysharmag9910-blip <rockysharmag9910@gmail.com>
2026-02-28 19:57:09 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
-const CACHE_NAME = 'pixel-perfect-v1';
-const ASSETS = [
-  './History.html',
+const CACHE_NAME = 'pixel-cache-v1';
+const urlsToCache = [
+  './index.html',
   './manifest.json'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
